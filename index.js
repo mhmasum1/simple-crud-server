21eea9d174a62211ebf68ac645f5b63bf6e5c2f9
mhmasum1 <mhmasum231@gmail.com>
2025-08-06 00:09:05 +0600

    Here i create a server that connect with client side and show the data in mongodb

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// mhmasum231
+// ZNWII6Z9kffRRGF8
+
+
+
+const uri = "mongodb+srv://mhmasum231:ZNWII6Z9kffRRGF8@cluster0.oeyfvq1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("usersDB");
+        const userCollection = database.collection("users")
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('SIMPLE CRUD ID RUNNING');
+})
+
+app.listen(port, () => {
+    console.log(`SIMPLE Crud is running on port: ${port}`)
+})
