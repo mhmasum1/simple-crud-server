8a5250d726d2141f77f2765a6cbf8a7d05294631
mhmasum1 <mhmasum231@gmail.com>
2025-08-16 12:50:07 +0600

    Read and delete operation added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -31,10 +31,24 @@ async function run() {
         const database = client.db("usersDB");
         const userCollection = database.collection("users")
 
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find()
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log('new user', user);
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log("Please delete from database", id);
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
